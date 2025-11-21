a1af3c6f913d476d5e639625a273b34469454d24
Mariano <marianogiardinamonti1@gmail.com>
2025-11-21 12:57:11 -0300

    Feature: seccion categorias terminada

--- app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/HomeActivity.java
+++ app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.glypha_primer_parcial_giardina_saracco;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,19 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textCatCards();
+    }
+
+    private void textCatCards(){
+        View cardModerna = findViewById(R.id.cardModerna);
+        View cardClasica = findViewById(R.id.cardClasica);
+        View cardElegante = findViewById(R.id.cardElegante);
+        View cardCreativa = findViewById(R.id.cardCreativa);
+
+        ((TextView) cardModerna.findViewById(R.id.txtCategoria)).setText(getString(R.string.txt_cat_moderna));
+        ((TextView) cardClasica.findViewById(R.id.txtCategoria)).setText(getString(R.string.txt_cat_clasica));
+        ((TextView) cardElegante.findViewById(R.id.txtCategoria)).setText(getString(R.string.txt_cat_elegante));
+        ((TextView) cardCreativa.findViewById(R.id.txtCategoria)).setText(getString(R.string.txt_cat_creativa));
     }
 }
