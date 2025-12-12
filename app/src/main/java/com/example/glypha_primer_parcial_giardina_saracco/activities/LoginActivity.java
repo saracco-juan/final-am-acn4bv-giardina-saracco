040a64d5e09b10dabb7f413a38f6da32c24772a4
Mariano <marianogiardinamonti1@gmail.com>
2025-12-12 00:09:21 -0300

    Feature: Login funcionality

--- app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/activities/LoginActivity.java
+++ app/src/main/java/com/example/glypha_primer_parcial_giardina_saracco/activities/LoginActivity.java
@@ -1,16 +1,28 @@
 package com.example.glypha_primer_parcial_giardina_saracco.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.glypha_primer_parcial_giardina_saracco.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +34,33 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void handleLogin(View v){
+
+        EditText mailInput = findViewById(R.id.mail_login_input);
+
+        EditText passInput = findViewById(R.id.password_login_input);
+
+        String mail = mailInput.getText().toString();
+
+        String password = passInput.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent home = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(home);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+        );
     }
 }
